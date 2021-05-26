3eaebaedd08460bae83e58b14460cdc8013f7dca
Olechka2018 <58590796+Olechka2018@users.noreply.github.com>
2021-05-26 11:54:08 +0700

    commit 1

--- app/src/main/java/com/example/myapplication88/Menu.kt
+++ app/src/main/java/com/example/myapplication88/Menu.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication88
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val start_calc = findViewById<Button>(R.id.b_start)
+        start_calc.setOnClickListener{
+            val perehod = Intent(this, calc::class.java )
+            startActivity(perehod)
+        }
+
+
     }
 }
